849c7930bbf5fa27c6100d7c5fe79cdbd5bb7a13
Satish Patil <satish.patil7017@gmail.com>
2022-11-03 14:43:47 +0530

    Added daily Employee Wage Solution

---
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.WriteLine("Welcome To Employee Wage Computation Problem");
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC2
+            DailyEmpWage prg = new DailyEmpWage();
+            prg.CheckDailyEmpWg();
+        }
+    }
+}
